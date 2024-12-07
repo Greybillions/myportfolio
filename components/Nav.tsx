2c7eb42eaa8a43e937fd9774cfcc9d2e177a25b2
Greybillions <olayile1999@gmail.com>
2024-12-08 00:21:54 +0100

    initial commit

---
+++ components/Nav.tsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <h1 className='text-6xl font-bold'>
+          Graham <span className='text-purple'>Boyle</span>
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
