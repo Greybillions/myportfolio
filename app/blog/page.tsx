544c8694086f20c1987fb540593635339f71acc9
Greybillions <olayile1999@gmail.com>
2025-01-03 04:40:05 +0100

    error bug fixed

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -127,6 +127,7 @@ const Blog = async () => {
       </div>
     );
   } catch (error) {
+    console.error('Error fetching posts:', error);
     return (
       <div className='p-6'>
         <h1 className='text-4xl font-bold mb-4'>My Blog</h1>
