ac470bba6c38b359fb5c6972bfe219fdf2cc913c
Greybillions <olayile1999@gmail.com>
2025-01-04 13:10:44 +0100

    created contact page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -48,8 +48,7 @@ const Nav = () => {
           </Link>
           <Link
             className='inline-flex items-center gap-2 text-[#c6ff00] bg-gray-900 px-4 py-1 rounded-lg font-semibold hover:bg-gray-800 hover:text-[#c6ff00] transition-all duration-300 ease-in-out hover:translate-x-1 hover:translate-y-1'
-            href='mailto:grahamboyle22@gmail.com'
-            target='_blank'
+            href='/contact'
             rel='noopener noreferrer'
           >
             Contact Me
@@ -104,8 +103,7 @@ const Nav = () => {
           </Link>
           <Link
             className='inline-flex text-center justify-center gap-2 text-[#c6ff00] bg-gray-900 px-4 py-1 rounded-lg font-semibold hover:bg-gray-800 hover:text-[#c6ff00] transition-all duration-300 ease-in-out hover:translate-x-1 hover:translate-y-1'
-            href='mailto:grahamboyle22@gmail.com'
-            target='_blank'
+            href='/contact'
             rel='noopener noreferrer'
           >
             Contact Me
